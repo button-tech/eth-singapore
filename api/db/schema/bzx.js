a004f004164e855cdeb9aed56899339b35f9a430
prazd <alexokda@list.ru>
2018-12-08 12:15:16 +0530

    fix api bzx schema

--- api/db/schema/bzx.js
+++ api/db/schema/bzx.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-const User = new Schema({
+const Orders = new Schema({
     _id: { 
         type: Schema.ObjectId,
         auto: true 
@@ -12,4 +12,4 @@ const User = new Schema({
     versionKey: false
 });
 
-module.exports = mongoose.model('User', User);
+module.exports = mongoose.model('Orders', Orders);
